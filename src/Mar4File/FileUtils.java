53b7421995de126d19fca07638e9ead8715dcfd5
Manish Madhusoodan <mmadhusoodan@marinsoftware.com>
2014-03-26 10:32:56 -0700

    added FileUtil file

---
+++ src/Mar4File/FileUtils.java
@@ -0,0 +1,23 @@
+package Mar4File;
+
+public class FileUtils {
+    
+    public FileUtils(){}
+    
+    boolean writeToFile(){
+        
+        return true;
+    }
+    
+    String readfromFile(){
+        
+        return "";
+    }
+    
+    public static void main (String args[]){
+        
+        FileUtils fu = new FileUtils();
+        fu.writeToFile();
+    }
+
+}
