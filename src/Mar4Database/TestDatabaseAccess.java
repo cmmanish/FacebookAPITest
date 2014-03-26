d0a7d05293547df9cac90582733b1440ff35489f
Manish Madhusoodan <mmadhusoodan@marinsoftware.com>
2014-03-26 10:34:12 -0700

    new test file

---
+++ src/Mar4Database/TestDatabaseAccess.java
@@ -0,0 +1,52 @@
+package Mar4Database;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+public class TestDatabaseAccess {
+    private final Logger log = Logger.getLogger(TestDatabaseAccess.class);
+
+    private AccessDatabase accessDb = new AccessDatabase();
+
+    @Test
+    public void testAccessToken() throws Exception {
+
+        String accessToken = accessDb.getFacebookAccount("53329000").get("access_token");
+        log.info(accessToken);
+
+    }
+
+    @Test
+    public void testReadFacebookCampaign() throws Exception {
+
+        Map<String, String> campaign = accessDb.readFacebookCampaign("Auto_PagePostBulkupload", 53329000);
+
+        if (campaign.size() == 0) {
+            log.info("Campaign Not found in Database ");    
+        }
+        
+        else {
+            log.info("campaignName: " + campaign.get("campaign_name"));
+            log.info("extId: " + campaign.get("ext_id"));
+            log.info("campaignStatus: " + campaign.get("campaign_status"));
+        }
+    }
+
+    @Test
+    public void testWriteFacebookCampaign() throws Exception {
+
+        accessDb.addFacebookCampaign("Auto_PagePostBulkupload", 53329000, 6016078203737l, "ACTIVE");
+
+    }
+    
+    @Test
+    public void testUpdateFacebookCampaign() throws Exception {
+
+        accessDb.updateFacebookCampaign("Auto_PagePostBulkupload", 53329000, "PAUSED");
+
+    }
+    
+
+}
